32186983d603c83abe47c4612cba668406e68806
Robert Jansen <me@rjns.dev>
2026-08-05 22:35:04 +0200

    update deps

--- shared/src/cache.rs
+++ shared/src/cache.rs
@@ -84,7 +84,7 @@ impl Cache {
             } => Some(Arc::new(
                 Client::connect(
                     format!(
-                        "redis-sentinel://{}/{cluster_name}/0",
+                        "redis-sentinel://{}/{cluster_name}/0?read_preference=prefer_replica",
                         redis_sentinels.join(",")
                     )
                     .as_str(),
